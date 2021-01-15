e91119afce2af0a33735502f0696fe85d58e7028
Colin Mudie <csmudie1@gmail.com>
2021-01-14 23:46:50 -0500

    added Current Weather API and UV index API to JS file

---
+++ script/script.js
@@ -0,0 +1,44 @@
+$("#searchBtn").on('click', function() {
+    let userInput = $('#userInput').val();
+    console.log(userInput);
+    let apiKey = '28095ae178a854ce629a96d482721f5d';
+    //Current Weather API
+    let cityNameURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + userInput + '&appid=' + apiKey
+    $.ajax({
+        url: cityNameURL,
+        method: 'GET'
+    })
+    .then(function (response){
+        console.log(response)
+        let cityName = response.city.name;
+        let date = response.list[0].dt_txt;
+        let weatherIcon = response.list[0].weather[0].icon;
+        let temperature = Math.round(9/5 * ((response.list[0].main.temp)-273) + 32);
+        let humidity = response.list[0].main.humidity;
+        let windSpeed = response.list[0].wind.speed;
+        let currentLat = response.city.coord.lat;
+        let currentLon = response.city.coord.lon;
+        
+        console.log('City: ' + cityName);
+        console.log('Date: ' + date);
+        console.log('Weather Icon: ' + weatherIcon);
+        console.log('Temperature: ' + temperature);
+        console.log('Humidity: ' + humidity);
+        console.log('Wind Speed: ' + windSpeed);
+        console.log('lat: ' + currentLat + ', long: ' + currentLon);
+
+        // UV Index API
+        let uvIndexURL = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + currentLat + '&lon=' + currentLon + '&appid=' + apiKey;
+        $.ajax({
+            url: uvIndexURL,
+            method: 'GET'
+        })
+        .then(function(response2){
+            console.log(response2);
+            let uvIndex = response2.value;
+            console.log('UV Index: ' + uvIndex);
+        })
+        
+
+    })
+});
