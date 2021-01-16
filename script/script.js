4c821c01bfe0de088dfe886008501f221c25718f
Colin Mudie <csmudie1@gmail.com>
2021-01-15 20:55:08 -0500

    added local storage functionality

--- script/script.js
+++ script/script.js
@@ -1,39 +1,43 @@
-$("#searchBtn").on('click', function () {
+let allEntries = JSON.parse(localStorage.getItem('all Entries'));
+if (allEntries === null) {
+    allEntries = [];
+}
 
-    let userInput = $('#userInput').val();
+
+function getThatWeather(userInput) {
     let apiKey = '28095ae178a854ce629a96d482721f5d';
-    
-    //Current Weather API
     let cityNameURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + userInput + '&appid=' + apiKey
-
+    //Current Weather API
     $.ajax({
         url: cityNameURL,
         method: 'GET'
     })
         .then(function (response) {
             console.log(response);
-            let cityName = response.city.name;
-            let date = response.list[0].dt_txt;
-            let weatherIcon = response.list[0].weather[0].icon;
-            let weatherIconURL = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
-            let temperature = Math.round(9 / 5 * ((response.list[0].main.temp) - 273) + 32);
-            let humidity = response.list[0].main.humidity;
-            let windSpeed = response.list[0].wind.speed;
-            let currentLat = response.city.coord.lat;
-            let currentLon = response.city.coord.lon;
+
+            let currentResponse = {
+                cityName: response.city.name,
+                date: response.list[0].dt_txt,
+                weatherIcon: "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png",
+                temperature: Math.round(9 / 5 * ((response.list[0].main.temp) - 273) + 32),
+                humidity: response.list[0].main.humidity,
+                windSpeed: response.list[0].wind.speed,
+                currentLat: response.city.coord.lat,
+                currentLon: response.city.coord.lon
+            }
 
             //Changing text content of Current Weather Card
-            $('#city-name').text(cityName);
-            $('#date').text(date);
-            $('#weather-icon').attr("src", weatherIconURL);
-            $('#temperature').text("Temperature: " + temperature);
-            $('#humidity').text(humidity);
-            $('#wind-speed').text("Wind Speed: " + windSpeed);
+            $('#city-name').text(currentResponse.cityName);
+            $('#date').text(currentResponse.date);
+            $('#weather-icon').attr("src", currentResponse.weatherIcon);
+            $('#temperature').text("Temperature: " + currentResponse.temperature);
+            $('#humidity').text("Humidity: " + currentResponse.humidity);
+            $('#wind-speed').text("Wind Speed: " + currentResponse.windSpeed);
 
 
 
             // UV Index API
-            let uvIndexURL = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + currentLat + '&lon=' + currentLon + '&appid=' + apiKey;
+            let uvIndexURL = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + currentResponse.currentLat + '&lon=' + currentResponse.currentLon + '&appid=' + apiKey;
             $.ajax({
                 url: uvIndexURL,
                 method: 'GET'
@@ -45,10 +49,10 @@ $("#searchBtn").on('click', function () {
                     if (uvIndex >= 3 && uvIndex < 6) {
                         $('#uv-color').attr("class", "badge badge-yellow");
                     }
-                    else if (uvIndex >= 6 && uvIndex < 8){
+                    else if (uvIndex >= 6 && uvIndex < 8) {
                         $('#uv-color').attr("class", "badge badge-orange");
                     }
-                    else if (uvIndex >= 8){
+                    else if (uvIndex >= 8) {
                         $('#uv-color').attr("class", "badge badge-red");
                     }
                     else {
@@ -58,31 +62,52 @@ $("#searchBtn").on('click', function () {
                     //displays weather card
                     $('#todayWeather').css('display', 'inline');
                 })
-            
+
 
             //5 Day Weather Forecast
             let six = 6;
             for (let i = 1; i < six; i++) {
-            let fiveDayDate = (response.list[i].dt_txt).split(" ");
-            let fiveDayWeatherIcon = response.list[i].weather[0].icon
-            let fiveDayWeatherIconURL = "http://openweathermap.org/img/wn/" + fiveDayWeatherIcon + "@2x.png";
-            let fiveDayTemperature = "Temperature: " + (Math.round(9 / 5 * ((response.list[i].main.temp) - 273) + 32));
-            let fiveDayHumidity = "Humidity: " + response.list[i].main.humidity;
-            let fiveDayCard = $(`
-                <div class="col-sm">
-                    <div class="card-body card">
-                        <h5 class="card-title">${fiveDayDate[0]}</h5>
-                        <div id="5day-icon-${i}"><img id="5day-weather-icon-${i}" src="${fiveDayWeatherIconURL}" alt="Weather Icon"></div>
-                        <p class="card-text">${fiveDayTemperature}</p>
-                        <p class="card-text">${fiveDayHumidity}</p>
-                    </div>
+                let fiveDayDate = (response.list[i].dt_txt).split(" ");
+                let fiveDayWeatherIcon = response.list[i].weather[0].icon
+                let fiveDayWeatherIconURL = "http://openweathermap.org/img/wn/" + fiveDayWeatherIcon + "@2x.png";
+                let fiveDayTemperature = "Temperature: " + (Math.round(9 / 5 * ((response.list[i].main.temp) - 273) + 32));
+                let fiveDayHumidity = "Humidity: " + response.list[i].main.humidity;
+                let fiveDayCard = $(`
+            <div class="col-sm">
+                <div class="card-body card">
+                    <h5 class="card-title">${fiveDayDate[0]}</h5>
+                    <div id="5day-icon-${i}"><img id="5day-weather-icon-${i}" src="${fiveDayWeatherIconURL}" alt="Weather Icon"></div>
+                    <p class="card-text">${fiveDayTemperature}</p>
+                    <p class="card-text">${fiveDayHumidity}</p>
                 </div>
-            `)
-            $('#5Day-Forecast').append(fiveDayCard);
-            console.log(fiveDayDate);
-            console.log(fiveDayWeatherIcon);
-            console.log(fiveDayTemperature);
-            console.log(fiveDayHumidity);
+            </div>
+        `)
+                $('#5Day-Forecast').append(fiveDayCard);
+            }
+
+            // Append Local Storage Name to List.
+            
+            $('.list-group').empty();
+            let savedEntries = JSON.parse(localStorage.getItem("all Entries"))
+            for (let index = 0; index < savedEntries.length; index++) {
+                let pastCityHTML = $(`
+                    <button type="button" class="list-group-item list-group-item-action">${savedEntries[index]}</button>
+                `)
+                $('.list-group').append(pastCityHTML);
+                
             }
         })
+}
+
+$("#searchBtn").on('click', function () {
+    let userInput = $('#userInput').val();
+    let currentInput = [userInput];
+    
+    localStorage.setItem(userInput, JSON.stringify(currentInput));
+    console.log(currentInput);
+    console.log(allEntries);
+    allEntries.push(currentInput);
+    localStorage.setItem("all Entries", JSON.stringify(allEntries));
+    console.log(allEntries);
+    getThatWeather(userInput)
 });
